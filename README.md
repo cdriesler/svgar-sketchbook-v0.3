5cdadc0a85cd680781efd1f19ef319f6007420da
chuck <cdriesler.iv@gmail.com>
2019-09-20 23:02:22 -0400

    add basic socket

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # opaque
-ui/ux and protocol sketches
+
+define-define was fun to make and interesting to get working. but it was boring. workshopping ui/ux ideas (and the tech necessary to pull them off) for a bit.
---
+++ api/src/server.ts
@@ -0,0 +1,23 @@
+import * as express from "express";
+import * as socketio from "socket.io";
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+// simple '/' endpoint sending a Hello World
+// response
+app.get("/", (req: any, res: any) => {
+    res.send("hello world");
+});
+
+io.on("connection", (socket: any) => {
+    console.log("connection!");
+});
+
+// start our simple server up on localhost:3000
+const server = http.listen(3000, function() {
+    console.log("listening on *:3000");
+});
