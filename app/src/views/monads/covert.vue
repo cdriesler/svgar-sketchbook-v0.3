3261fdb16b815c792e8f3e5911778b5a7900fee4
chuck <cdriesler.iv@gmail.com>
2019-10-10 22:58:33 -0400

    breathing shapes

--- app/src/views/monads/covert.vue
+++ app/src/views/monads/covert.vue
@@ -1,8 +1,6 @@
 <template>
 <div id="covert">
-    <div class="white box" v-html="svg">
-    </div>
-    <div class="black box">
+    <div class="white box" ref="svgar" v-html="svg">
     </div>
 </div>
 </template>
@@ -20,7 +18,9 @@
 
 .box {
     width: 400px;
+    max-width: calc(100vw - 60px);
     height: 400px;
+    max-height: calc(100vw - 60px);
 }
 
 .white {
@@ -38,33 +38,104 @@ import Vue from 'vue'
 import Svgar from 'svgar';
 import SvgarPath from 'svgar/dist/models/SvgarPath';
 
+interface Circle {
+    x: number,
+    y: number,
+    r: number,
+}
+
+interface Rectangle {
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+    rot: number,
+}
+
 export default Vue.extend({
     name: 'covert',
     data() {
         return {
             offset: 0,
             w: 0.013,
+            circle: {} as Circle,
+            rectangle: {} as Rectangle,
+            s: 0,
+            angle: 0,
+            multiplier: 0,
         }
     },
     mounted() {
         setInterval(this.shiftOffset, 25);
+        setInterval(this.shiftSize, 25);
+        this.circle = {
+            x: 0.25,
+            y: 0.3,
+            r: 0.2
+        }
+        this.rectangle = {
+            x: 0.7,
+            y: 0.6,
+            w: 0.15,
+            h: 0.65,
+            rot: 30,
+        }
+        setInterval(this.shiftShapes, 25);
+        this.s = (<Element>this.$refs.svgar).clientWidth;
     },
     computed: {
         svg(): string {
             let dwg = new Svgar.Cube();
 
-            let block = new Svgar.Slab("blocks");
-            let r = this.makeRectangle(0.5, 0.5, 0.1, 0.8, 30);
-
-            let front = new Svgar.Slab("front");
-
-            let circle = new Svgar.Builder.Circle(0.25, 0.25, 0.26).build();
             let background = new Svgar.Builder.Polyline(0, 0).lineTo(1, 0).lineTo(1, 1).lineTo(0, 1).close().build();
             background.setElevation(-1);
             background.setTag("background");
 
+            let block = new Svgar.Slab("blocks");
+            let r = this.rectangle as Rectangle;
+            let rect = this.makeRectangle(r.x, r.y, r.w, r.h, r.rot);
+
+            block.clipWith(rect);
+            block.setElevation(2);
+
+            block.setAllStyles([
+                {
+                    name: "default",
+                    attributes: {
+                        "fill": "black",
+                        "stroke": "none",
+                        "stroke-width": "0px",
+                    },
+                },
+                {
+                    name: "bg",
+                    attributes: {
+                        "fill": "white",
+                        "stroke": "none",
+                        "stroke-width": "0px",
+                    }
+                },
+            ]);
+
+            block.setAllStates([
+                {
+                    name: "default",
+                    styles: {
+                        "background": "bg",
+                        "screen": "bg",
+                    }
+                }
+            ])
+
+            block.setAllGeometry([...this.getHorizontalStripes(this.w, 0), background])
+
+            let front = new Svgar.Slab("front");
+
+            const c = this.circle as Circle;
+            let circle = new Svgar.Builder.Circle(c.x, c.y, c.r).build();
+
             front.setAllGeometry([...this.getVerticalStripes(this.w, this.offset + 1), background]);
-            front.clipWith(r);
+            front.clipWith(circle);
             front.setElevation(2);
 
             front.setAllStyles([
@@ -136,9 +207,9 @@ export default Vue.extend({
 
             back.setElevation(1);
 
-            dwg.slabs.push(front, back);
+            dwg.slabs = [front, back, block];
 
-            return dwg.compile(400, 400);
+            return dwg.compile(this.s, this.s);
         }
     },
     methods: {
@@ -147,6 +218,39 @@ export default Vue.extend({
 
             if (this.offset > 2) {
                 this.offset = 0;
+            }
+        },
+        shiftShapes(): void {
+            let speed = 0.0015;
+
+            this.circle.x = 0.25 + (0.1 * this.multiplier);
+            this.circle.r = 0.15 + (0.075 * this.multiplier);
+
+            this.rectangle.x = 0.7 - (0.1 * this.multiplier);
+            this.rectangle.w = 0.15 + (0.05 * this.multiplier);
+            this.rectangle.rot = 30 + (15 * this.multiplier);
+
+        },
+        shiftSize(): void {
+            this.angle = this.angle + 0.1;
+            this.multiplier = Math.sin(this.angle * (Math.PI / 18));
+
+            //this.w = Math.abs((Math.sin(this.angle * (Math.PI / 180))) * 0.025) + 0.025
+        },
+        newCircle(): Circle {
+            return {
+                x: Math.random() * 0.3 + 0.1,
+                y: 1.5,
+                r: Math.random() * 0.25 + 0.05
+            }
+        },
+        newRectangle(): Rectangle {
+            return {
+                x: Math.random() * 0.3 + 0.5,
+                y: 1.5,
+                w: Math.random() * 0.15 + 0.05,
+                h: Math.random() * 0.4 + 0.4,
+                rot: (Math.random() * 115) - 45,
             }
         },
         getVerticalStripes(w: number, pct: number): SvgarPath[] {
